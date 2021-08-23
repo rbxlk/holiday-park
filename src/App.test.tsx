8defed952a61cb0908398b98c720be1ca8054b70
rbxlk <rbxlk@protonmail.com>
2021-08-23 13:28:35 +0100

    Fix broken text search test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders About menu item', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/About/i);
   expect(linkElement).toBeInTheDocument();
 });
